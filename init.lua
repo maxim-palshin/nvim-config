d8e9c390352375f5b3a62d99c211fb6a5c3196fa
Palshin Maxim <palshin.maxim.alekseevich@gmail.com>
2024-09-14 14:08:16 +0000

    remove debug

--- init.lua
+++ init.lua
@@ -136,53 +136,6 @@ require("lazy").setup({
     end
   },
 
-  -- Отладка
-  {
-    "mfussenegger/nvim-dap",
-    config = function()
-      local dap = require("dap")
-      dap.adapters.lldb = {
-        type = 'executable',
-        command = 'lldb-vscode', -- adjust as needed
-        name = "lldb"
-      }
-      dap.configurations.cpp = {
-        {
-          name = "Launch",
-          type = "lldb",
-          request = "launch",
-          program = function()
-            return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
-          end,
-          cwd = '${workspaceFolder}',
-          stopOnEntry = false,
-          args = {},
-          runInTerminal = false,
-        },
-      }
-    end
-  },
-
-  -- nvim-dap-ui
-  {
-    "rcarriga/nvim-dap-ui",
-    dependencies = { "mfussenegger/nvim-dap","nvim-nio" },
-    config = function()
-      local dap, dapui = require("dap"), require("dapui")
-      dapui.setup()
-
-      dap.listeners.after.event_initialized["dapui_config"] = function()
-        dapui.open()
-      end
-      dap.listeners.before.event_terminated["dapui_config"] = function()
-        dapui.close()
-      end
-      dap.listeners.before.event_exited["dapui_config"] = function()
-        dapui.close()
-      end
-    end
-  },
-
   -- CMake
   {
     "Civitasv/cmake-tools.nvim",
